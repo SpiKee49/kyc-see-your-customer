d60876de2697aad814d0a86519c7c375348a10e4
René Bukovina <r.bukovina@gmail.com>
2024-04-21 14:55:28 +0200

    scaler added

--- src/pages/DocumentScan.tsx
+++ src/pages/DocumentScan.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from "react"
-import { OcrInstance } from "../axios"
+// import { OcrInstance } from "../axios"
+import { createWorker } from "tesseract.js"
+import Upscaler from "upscaler"
+import deblur from "@upscalerjs/maxim-deblurring"
 
-const SCREENSHOT_TIME_INTERVAL_MS = 4000
-// const VIDEO_HEIGHT = 720
-// const VIDEO_WIDTH = 1280
+// const SCREENSHOT_TIME_INTERVAL_MS = 4000
 
 function DocumentScan() {
   const videoRef = useRef<HTMLVideoElement>(null)
@@ -11,66 +12,70 @@ function DocumentScan() {
   const [foundText, setFoundText] = useState("")
 
   const [screenshot, setScreenshot] = useState<string>()
-  const maskRef = useRef<HTMLCanvasElement>(null)
+  const [deblured, setDeblur] = useState<string>()
 
   async function getScreenshot() {
-    if (maskRef.current == null) return
-
-    const ctx = maskRef.current.getContext("2d")
+    const upscaler = new Upscaler({ model: deblur })
+    upscaler.warmup({ patchSize: 512, padding: 2 })
+    const canvas = document.createElement("canvas")
+    canvas.width = 1280
+    canvas.height = 720
+    const ctx = canvas.getContext("2d")
     if (ctx == null || ocrIsRunning || !videoRef.current) return
-    const videoRect = videoRef.current.getBoundingClientRect()
-    const maskRect = maskRef.current.getBoundingClientRect()
-
-    maskRef.current.width = videoRect.width
-    maskRef.current.height = videoRect.height
-
-    ctx.drawImage(videoRef.current, 0, 0)
-    let data = maskRef.current.toDataURL("image/png", 1)
+    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height)
+    let data = canvas.toDataURL("image/png", 1)
     setScreenshot(data)
-    runOCR(data)
+    const image = new Image()
+    image.src = data
+
+    const result = await upscaler.upscale(image, {
+      output: "base64",
+      patchSize: 512,
+      padding: 2,
+      progress: progress => console.log(progress)
+    })
+    setDeblur(result)
+    // const API_URL = "127.0.0.1/upload"
+
+    // const res = await axios.post(API_URL, { base64Img: data })
+    // console.log(JSON.stringify(res.data))
+
+    // runOCR(data)
   }
 
-  function streamCamVideo() {
-    const constraints = {
-      audio: false,
-      video: {
-        // width: VIDEO_WIDTH,
-        // height: VIDEO_HEIGHT,
-        facingMode: "environment"
-      }
-    }
+  // function streamCamVideo() {
+  //   const constraints = {
+  //     audio: false,
+  //     video: {
+  //       facingMode: "environment"
+  //     }
+  //   }
 
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(async mediaStream => {
-        const video = videoRef.current
+  //   navigator.mediaDevices
+  //     .getUserMedia(constraints)
+  //     .then(async mediaStream => {
+  //       const video = videoRef.current
 
-        if (video === null) {
-          console.log("video null")
-          return
-        }
+  //       if (video === null) {
+  //         console.log("video null")
+  //         return
+  //       }
 
-        video.srcObject = mediaStream
-        video.onloadedmetadata = () => video.play()
-      })
-      .catch(function (err) {
-        console.log(err.name + ": " + err.message)
-      }) // always check for errors at the end.
-  }
+  //       video.srcObject = mediaStream
+  //       video.onloadedmetadata = () => video.play()
+  //     })
+  //     .catch(function (err) {
+  //       console.log(err.name + ": " + err.message)
+  //     }) // always check for errors at the end.
+  // }
 
   async function runOCR(screenshotBase64: string) {
     setOcrIsRunning(true)
     try {
-      const formData = new FormData()
-      formData.append("base64Image", screenshotBase64)
-      formData.append("language", "cze")
-      formData.append("scale", "true")
-      formData.append("apikey", import.meta.env.VITE_API_KEY)
-
-      const res = await OcrInstance.post("", formData)
-      const text = res.data.ParsedResults
-      setFoundText(JSON.stringify(text))
-      console.log(text)
+      const worker = await createWorker("slk")
+      const ret = await worker.recognize(screenshotBase64)
+      console.log(ret.data.text)
+      await worker.terminate()
     } catch (error) {
       console.log(error)
     }
@@ -79,12 +84,12 @@ function DocumentScan() {
   }
 
   useEffect(() => {
-    streamCamVideo()
-    const screenShotInterval = setInterval(() => {},
-    SCREENSHOT_TIME_INTERVAL_MS)
+    // streamCamVideo()
+    // const screenShotInterval = setInterval(() => {},
+    // SCREENSHOT_TIME_INTERVAL_MS)
 
     return () => {
-      clearInterval(screenShotInterval)
+      // clearInterval(screenShotInterval)
       videoRef.current?.pause()
     }
   }, [])
@@ -94,10 +99,7 @@ function DocumentScan() {
       <h1>Video</h1>
       <div className={`relative w-full`}>
         <video className="w-full" autoPlay={true} ref={videoRef}></video>
-        <canvas
-          className="absolute border-2 w-1/2 h-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  opacity-0"
-          ref={maskRef}
-        ></canvas>
+
         <div className="absolute border-2 w-1/2 h-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "></div>
         <span className="absolute bottom-0 left-1/2 -translate-x-1/2 text-white font-light text-center bg-black bg-opacity-25">
           {ocrIsRunning
@@ -117,6 +119,12 @@ function DocumentScan() {
         <>
           <h1>Screenshot </h1>
           <img src={screenshot} />
+        </>
+      )}
+      {deblured && (
+        <>
+          <h1>deblured </h1>
+          <img src={deblured} />
         </>
       )}
     </div>
