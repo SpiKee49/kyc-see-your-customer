167b0f4e038fe45cf7c2e37337dabd9e08a5b7d7
René Bukovina <r.bukovina@gmail.com>
2024-05-07 14:10:39 +0200

    feat(api): status return null

--- server/index.ts
+++ server/index.ts
@@ -78,14 +78,8 @@ app.get(
                     status: true
                 }
             })
-            if (verification == null) {
-                res.status(404).json({
-                    message: "Verification with such birth number doesnt exist."
-                })
-                return
-            }
 
-            res.status(200).json(verification.status)
+            res.status(200).json(verification?.status ?? null)
         } catch (err) {
             res.status(500).json({ message: "Internal server error" })
             console.error(err)
